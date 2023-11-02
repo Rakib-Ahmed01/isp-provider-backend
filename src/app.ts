92cd013295011fd3ffc08eeca692e3dd4ddcccdb
RakibAhmed01 <rakibahmed34360@gmail.com>
2023-11-03 01:52:29 +0600

    feat: ratelimiter and swagger docs added (not complete)

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,62 @@
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
+import { rateLimit } from 'express-rate-limit';
+import swaggerUi from 'swagger-ui-express';
+
+import fs from 'fs';
+import path from 'path';
+import YAML from 'yaml';
 import { globalErrorHandler, notFoundHandler } from './middlewares/errors';
 import { router } from './routes';
+import throwApiError from './utils/throwApiError';
 
 const app: Application = express();
 
+const file = fs.readFileSync(path.resolve(__dirname, 'swagger.yaml'), 'utf8');
+const swaggerDocument = YAML.parse(file);
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 500,
+  standardHeaders: true,
+  legacyHeaders: false,
+  handler: (_, __, ___, options) => {
+    throwApiError(
+      options.statusCode || 500,
+      `There are too many requests. You are only allowed ${
+        options.max
+      } requests per ${options.windowMs / 60000} minutes`,
+    );
+  },
+});
+
 app.use([
   cors(),
+  limiter,
   express.json(),
   express.urlencoded({ extended: true }),
   cookieParser(),
 ]);
 
 // Home Routes
-app.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'Welcome to Book Catalog Server...🐱‍🏍' });
-});
+// app.get('/', (req: Request, res: Response) => {
+//   res.json({ message: 'Welcome to Book Catalog Server...🐱‍🏍' });
+// });
 
 app.get('/api/v1', (req: Request, res: Response) => {
   res.json({ message: 'Welcome to Book Catalog Server...🐱‍🏍' });
 });
 
 app.use('/api/v1', router);
+
+app.use(
+  '/',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, {
+    customSiteTitle: 'QuickNet API Documentation',
+  }),
+);
 
 // error handlers
 app.use(notFoundHandler);
